a3848f89dc815b7ea16302bcecfff573ba8a75ac
Anton O <noname@noemail>
2017-11-24 16:57:33 +0300

    init

--- tests/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require __DIR__ . '/../vendor/autoload.php';
-
-define("ROOT", realpath('..'));
-define("TESTS", ROOT . "/tests");
--- typpc.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require "vendor/autoload.php";
-
-$compiler = new \Typp\Compiler();
